8f998dbedb2e13bf502cb43014294d038e960a3b
LAB <LAB@LAB-49-PC01>
2023-10-18 01:57:55 -0700

    Add project files.

---
+++ BernalAreaProject/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BernalAreaProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int length;
+            int width;
+
+
+            int area;
+
+            Console.Write("Enter Length:");
+            length = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter width:");
+            width = int.Parse(Console.ReadLine());
+
+            area = (length * width);
+
+            Console.WriteLine($"area: {area}");
+
+
+
+        }
+    }
+}
